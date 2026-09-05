6492dd1edfad063687bc4c8f23de2a056734afea
Pedro Bizachi <pedro_bizachi@outlook.com>
2026-09-05 17:17:16 -0300

    feat: No auto continue comments on new line

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -12,6 +12,14 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
 	end,
 })
 
+-- no auto continue comments on new line
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
+
 -- Start recording macro
 vim.api.nvim_create_autocmd("RecordingEnter", {
 	desc = "Simple notify when recording a macro",
